7b33d71e51aa7ba5df292a09702040718a3fd2b1
NataliaP14 <pegueronatalia@gmail.com>
2023-11-04 13:46:49 -0400

    First Commit

---
+++ HackNJITQuizGame/src/QuizMain.java
@@ -0,0 +1,13 @@
+
+public class QuizMain {
+
+	public static void main(String[] args) {
+		
+		MarineLife marine = new MarineLife();
+		Ports port = new Ports();
+		OceanFacts oceanF = new OceanFacts();
+		ShipFacts ship = new ShipFacts();
+
+	}
+
+}
