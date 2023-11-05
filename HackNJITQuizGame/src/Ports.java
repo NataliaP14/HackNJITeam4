79681a911ce29b5ffc5d9517527d6b78c3f1f0bb
NataliaP14 <140302491+NataliaP14@users.noreply.github.com>
2023-11-05 01:40:20 -0500

    Update Ports.java

--- HackNJITQuizGame/src/Ports.java
+++ HackNJITQuizGame/src/Ports.java
@@ -1,31 +1,9 @@
-OliviaBranch
+
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Set;
 
-public class Ports {
-
-	public void displayQuestionP(String randomQuestionP) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public String getRandomQuestionP() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public boolean checkAnswerP(String randomQuestionP, String answerP) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public int getPortsIncremenet() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
-	    
+public class Ports {  
 	int portsIncrement = 0;
 	private Random randomP = new Random();
 	private HashMap<String, String> questionsP = new HashMap<>();
@@ -97,7 +75,7 @@ public class Ports {
 
 public class Ports {
 	
-main
+
 }
 	
 
